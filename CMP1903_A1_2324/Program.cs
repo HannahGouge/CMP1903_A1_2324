087c0289fe9f0dd50ba7b73bfad5296c0840b14e
HannahGouge <158061843+HannahGouge@users.noreply.github.com>
2024-03-01 17:59:17 +0000

    Update Program.cs

--- CMP1903_A1_2324/Program.cs
+++ CMP1903_A1_2324/Program.cs
@@ -14,6 +14,8 @@ namespace CMP1903_A1_2324
              * Create a Game object and call its methods.
              * Create a Testing object to verify the output and operation of the other classes.
              */
+            Game g1 = new Game()
+            Testing t1 = new Testing()
         }
     }
 }
